e323c90413aaee2ba99acd083e6d15c80e013374
Kshitij Patil <kshitijpatil98@gmail.com>
2020-09-02 03:38:31 +0530

    Add setup files for packaging
    * Added setup script for building package
    * Updated project & test requirements
    * Primary setup fields were defined inside peddet/__init_.py
    
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

---
+++ peddet/__init__.py
@@ -0,0 +1,9 @@
+"""Root package info."""
+
+__version__ = '0.1.1'
+__author__ = 'Kshitij Patil'
+__author_email__ = 'kshitijpatil98@gmail.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (c)  2020-2020, %s.' % __author__
+__homepage__ = 'https://github.com/Kshitij09/pedestrian-detection'
+__docs__ = 'Structured implementation of pedestrian detection API using pytorch, pytorch-lightning and hydra'
