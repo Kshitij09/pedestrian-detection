3144dff47de4429e8ca40c7f51276fda008c0954
Kshitij Patil <kshitijpatil98@gmail.com>
2020-10-03 02:41:20 +0530

    Add cocoapi metric utils
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- peddet/utils/utils.py
+++ peddet/utils/utils.py
@@ -1,7 +1,80 @@
+# Direct copy from https://github.com/pytorch/vision/blob/master/references/detection/utils.py
+
+import torch
+import pickle
+import torch.distributed as dist
 from omegaconf import OmegaConf
 import collections
+
+
 def collate_fn(batch):
     return tuple(zip(*batch))
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def all_gather(data):
+    """
+    Run all_gather on arbitrary picklable data (not necessarily tensors)
+    Args:
+        data: any picklable object
+    Returns:
+        list[data]: list of data gathered from each rank
+    """
+    world_size = get_world_size()
+    if world_size == 1:
+        return [data]
+
+    # serialized to a Tensor
+    buffer = pickle.dumps(data)
+    storage = torch.ByteStorage.from_buffer(buffer)
+    tensor = torch.ByteTensor(storage).to("cuda")
+
+    # obtain Tensor size of each rank
+    local_size = torch.tensor([tensor.numel()], device="cuda")
+    size_list = [torch.tensor([0], device="cuda") for _ in range(world_size)]
+    dist.all_gather(size_list, local_size)
+    size_list = [int(size.item()) for size in size_list]
+    max_size = max(size_list)
+
+    # receiving Tensor from all ranks
+    # we pad the tensor because torch all_gather does not support
+    # gathering tensors of different shapes
+    tensor_list = []
+    for _ in size_list:
+        tensor_list.append(torch.empty((max_size,), dtype=torch.uint8, device="cuda"))
+    if local_size != max_size:
+        padding = torch.empty(
+            size=(max_size - local_size,), dtype=torch.uint8, device="cuda"
+        )
+        tensor = torch.cat((tensor, padding), dim=0)
+    dist.all_gather(tensor_list, tensor)
+
+    data_list = []
+    for size, tensor in zip(size_list, tensor_list):
+        buffer = tensor.cpu().numpy().tobytes()[:size]
+        data_list.append(pickle.loads(buffer))
+
+    return data_list
+
+
+class IOU_TYPES:
+    BBOX = "bbox"
+    MASK = "segm"
+    KEYPOINT = "keypoints"
+
 
 # Borrowed from
 # https://github.com/Erlemar/wheat/blob/54d253cc7ff559bebc1056dca8b5b058fb75fc9f/src/utils/utils.py#L110
